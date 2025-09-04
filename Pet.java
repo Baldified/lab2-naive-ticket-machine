ef486d366a658b912611a93561b5e79267179f93
Bruce <l4m.bruc3@gmail.com>
2025-09-03 20:32:29 -0400

    step 21

---
+++ Pet.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Pet here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Pet
+{
+    // instance variables - replace the example below with your own
+    private String name;
+
+    /**
+     * Constructor for objects of class Pet
+     */
+    public Pet(String petsName)
+    {
+        // initialise instance variables
+        petsName = name;
+    }
+ 
+}
