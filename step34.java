a9ee41466feea1bcd3ccf4a41ad2108f0753ce46
Bruce <l4m.bruc3@gmail.com>
2025-09-03 21:40:26 -0400

    step 34

---
+++ step34.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class step34 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class step34
+{
+    // instance variables - replace the example below with your own
+    private int age;
+
+    /**
+     * Constructor for objects of class step34
+     */
+    public step34()
+    {
+        age = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int setAge(int currentAge)
+    {
+        age = currentAge;
+        return age;
+    }
+}
