eeffd2f4e5fc85c999b7874e5b1e1f388a9fc578
Bruce <l4m.bruc3@gmail.com>
2025-09-03 20:56:35 -0400

    did step 25

--- TicketMachine.java
+++ TicketMachine.java
@@ -42,7 +42,7 @@
      * Return the amount of money already inserted for the
      * next ticket.
      */
-    public int getBalance()
+    public int getAmount()
     {
         return balance;
     }
