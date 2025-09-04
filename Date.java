a0e2b17d170107d2e5d1750d7b2ee544fa10e42d
Bruce <l4m.bruc3@gmail.com>
2025-09-03 20:42:54 -0400

    updated step 21 and did step 22

---
+++ Date.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Date here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Date
+{
+    // instance variables - replace the example below with your own
+    private String time;
+
+    /**
+     * Constructor for objects of class Date
+     */
+    public Date(String setDate)
+    {
+        time = setDate;
+    }
+
+}
--- Pet.java
+++ Pet.java
@@ -16,7 +16,7 @@ public class Pet
     public Pet(String petsName)
     {
         // initialise instance variables
-        petsName = name;
+        name = petsName;
     }
  
 }
